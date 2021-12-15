37a7ce5888f8691e746ea0faa4d691aed763dee0
dialloflatno <brahimdiallo23@gmail.com>
2021-12-15 02:31:38 -0500

    commit tree c1da24ea26ea7aa41e2d6873f35a3e1332afcc82

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,8 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:id', to: 'cheeses#show'
+
 end
