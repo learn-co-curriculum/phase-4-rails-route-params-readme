d39e1eb1e9094a1848b4018a588f769c3f4c5747
Zac-Mwangi <zackmwangi998@gmail.com>
2022-09-26 21:01:03 +0300

    commit tree bf9730878b732ab008c35259ef281a30459e2782

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,9 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+
+  get '/cheeses/:id', to: 'cheeses#show'
+
 end
